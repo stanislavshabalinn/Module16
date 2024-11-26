87050c3854aa89ed2aaff6ea8d806a188156cab0
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-11-26 22:34:58 +0700

    Добавьте файлы проекта.

---
+++ Calculator.Tests/Calculator.Tests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Module16
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(20, 10), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(20, 10), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(20, 10), Is.EqualTo(200));
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(20, 10), Is.EqualTo(2));
+        }
+
+    }
+
+}
