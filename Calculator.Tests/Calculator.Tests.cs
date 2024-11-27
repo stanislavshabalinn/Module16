5dc0763a558b078592f225ac2ed869d85e37f24c
shabalin_sa1@mail.ru <shabalin_sa1@mail.ru>
2024-11-27 13:38:50 +0700

    готово

--- Calculator.Tests/Calculator.Tests.cs
+++ Calculator.Tests/Calculator.Tests.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
-using Assert = NUnit.Framework.Assert;
 
 namespace Module16
 {
